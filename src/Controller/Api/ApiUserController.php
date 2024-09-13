c779f54db376dd8ec203d690e7f98f8d865392f8
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2024-09-13 09:23:39 +0200

    feat: Add ApiUserController

---
+++ src/Controller/Api/ApiUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Form\RegistrationFormType;
+
+
+class ApiUserController extends AbstractController
+{
+    #[Route('/api/users', name: 'api_users', methods: ['GET'])]
+    public function index(UserRepository $userRepo): JsonResponse
+    {
+        $users = $userRepo->findAll();
+        $users = array_map(function(User $user) {
+            return [
+                'id'    =>  $user->getId(),
+                'email' =>  $user->getEmail()
+            ];
+        }, $users);
+        return new JsonResponse($users);
+    }
+
+    #[Route('/api/add_user', name: 'api_add_user', methods: ['POST'])]
+    public function addUser(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): JsonResponse
+    {
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return new JsonResponse(['status' => 'Error'], 403);
+        }
+        try {
+            $params = json_decode($request->getContent(), true);
+            $user = new User();
+            $user->setEmail($params['email']);
+            $user->setPassword($userPasswordHasher->hashPassword($user, $params['password']));
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return new JsonResponse(['status' => 'User created!'], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['status' => 'Error', "message" => $e->getmessage()], 400);
+        }
+    }
+}
