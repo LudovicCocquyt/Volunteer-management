57146441ff9c049be19cb34effa7d7f6bfc3e177
lcocquyt <ludovic.cocquyt@netoxygen.ch>
2025-07-25 13:41:47 +0200

    feat: Creation component for managing screen width

---
+++ assets/preact/utils/UseWindowSize.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'preact/hooks';
+
+export const UseWindowSize = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return width;
+};
